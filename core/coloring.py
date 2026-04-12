0aa3225d79a07de9df23ed4cbee3c70c79344662
Jyotish Pabbisetti <jyotishpabbisetti2025@gmail.com>
2026-04-12 12:26:33 +0530

    Implement bus assignments using graph coloring
    Add bus assignment printing based on graph coloring.

--- core/coloring.py
+++ core/coloring.py
@@ -1,1 +1,10 @@
 
+coloring_dict = nx.greedy_color(G, strategy='largest_first')
+node_colors = [(node,coloring_dict[node]) for node in G.nodes()]
+print("\nBus Assignments:")
+for node in coloring_dict:
+    print(f"{node} → Bus {coloring_dict[node] + 1}")
+
+  
+  
+
