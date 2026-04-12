b923aeac4310e58b6888ebe3f9ea9abff11bb187
Jyotish Pabbisetti <jyotishpabbisetti2025@gmail.com>
2026-04-12 12:27:52 +0530

    Initialize graph with nodes and edges using NetworkX

--- core/graph_builder.py
+++ core/graph_builder.py
@@ -1,1 +1,16 @@
+import networkx as nx
+
+
+G = nx.Graph()
+
+# Add nodes
+for i in dat:
+    G.add_node(i["id"])
+
+# Add edges
+for i in results:
+    if i[2]:  # boolean check
+        G.add_edge(i[0], i[1])
+  
+  
 
